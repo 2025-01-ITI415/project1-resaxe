069f0efab743a53445cbeedc82905eb66554e47e
Ruby <ruby.saxe@gmail.com>
2025-02-24 13:48:05 -0500

    made it return to start when not touching a log or dock
    collisions are a pain but i learned how to use OnTriggerStay and OnTriggerExit

--- Assets/__Scripts/RubberDuck.cs
+++ Assets/__Scripts/RubberDuck.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -7,15 +8,17 @@ using UnityEngine.UIElements;
 
 public class RubberDuck : MonoBehaviour
 {
-    
-    
-    
-    
-    
+    public Vector3 startPosition;
+    public Quaternion startRotation;
+    public Boolean falling = false;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
+        startRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -41,6 +44,13 @@ public class RubberDuck : MonoBehaviour
             transform.rotation = Quaternion.Euler(-90f, 0f, -90f);
             Move(Vector3.left);
         }
+
+        if (falling == true)
+        {
+            gameObject.transform.SetPositionAndRotation(startPosition, startRotation);
+            falling = false;
+        }
+
     }
 
     private void Move(Vector3 direction)
@@ -48,20 +58,42 @@ public class RubberDuck : MonoBehaviour
         transform.position += direction*4;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider collision)
     {
-        // seperate barrier tags because i couldnt get one if statement to work for all of them
-        if (other.gameObject.CompareTag("LeftBarrier"))
+
+        // seperate barrier tags because i couldnt get one if statement to work for the different sides
+        if (collision.gameObject.CompareTag("LeftBarrier"))
         {
             Move(Vector3.right);
         }
-        else if (other.gameObject.CompareTag("RightBarrier"))
+        else if (collision.gameObject.CompareTag("RightBarrier"))
         {
             Move(Vector3.left);
         }
-        else if (other.gameObject.CompareTag("BottomBarrier"))
+        else if (collision.gameObject.CompareTag("BottomBarrier"))
         {
             Move(Vector3.forward);
+        }
+
+
+        
+    }
+
+    // the docks and all logs have a safe zone tag, this checks if its touching a log or dock
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "SafeZone")
+        {
+            falling = false;
+        }
+    }
+
+    // checks if it's no longer interacting with a dock or log, aka falling in the water
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == "SafeZone")
+        {
+            falling = true;
         }
     }
 
